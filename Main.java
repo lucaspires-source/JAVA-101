06054a5f86ffa997f6f34b52b3245151a5907837
lucaspires-source <pires.lucas94@gmail.com>
2022-06-07 16:36:32 -0300

    MATRIX

--- Main.java
+++ Main.java
@@ -2,18 +2,9 @@ import java.util.Arrays;
 
 public class Main {
    public static void main(String[] args) {
-    int[] numbers = new int[5];
-    numbers[0]= 1;
-    numbers[1]= 2;
-    numbers[2]= 3;
-    numbers[3]= 4;
-    numbers[4]= 5;
-    
-    System.out.println(numbers);
-    System.out.println(Arrays.toString(numbers));
+    int[][] numbers = new int[2][3];
+    numbers[0][0] = 1;
 
-
-    int[] newNumbers = {2,3,4,5,1,6};
-    System.out.println(numbers.length);
+    System.out.println(Arrays.deepToString(numbers));
  }
 }
