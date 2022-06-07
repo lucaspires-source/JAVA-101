e12ce992048f50c7dfe50a76e17a58e5ad8acf48
lucaspires-source <pires.lucas94@gmail.com>
2022-06-07 16:21:42 -0300

    Escape sequences

--- Main.java
+++ Main.java
@@ -2,15 +2,7 @@
 
 public class Main {
    public static void main(String[] args) {
-    String name = "Lucas Ribeiro" + " Pires";
-
-    System.out.println(name);
-    System.out.println(name.startsWith("Pires"));
-    System.out.println(name.length());
-    System.out.println(name.indexOf('a'));
-    System.out.println(name.replace('a' , '8'));
-    System.out.println(name.toLowerCase());
-    System.out.println(name.toUpperCase());
-    System.out.println(name.trim());
+    String name = "Lucas \\Ribeiro\\ Pires";
+   System.out.println(name);
  }
 }
