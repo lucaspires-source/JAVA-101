788107746299c0d5876ec513ba5f2e4909edb4ad
lucaspires-source <pires.lucas94@gmail.com>
2022-06-07 16:33:26 -0300

    Arrays

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
-
+import java.util.Arrays;
 
 public class Main {
    public static void main(String[] args) {
-    String name = "Lucas \\Ribeiro\\ Pires";
-   System.out.println(name);
+    int[] numbers = new int[5];
+    numbers[0]= 1;
+    numbers[1]= 2;
+    numbers[2]= 3;
+    numbers[3]= 4;
+    numbers[4]= 5;
+    
+    System.out.println(numbers);
+    System.out.println(Arrays.toString(numbers));
+
+
+    int[] newNumbers = {2,3,4,5,1,6};
+    System.out.println(numbers.length);
  }
 }
