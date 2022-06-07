250a69dd0482dae565284b61ab97f683aa72559e
lucaspires-source <pires.lucas94@gmail.com>
2022-06-07 15:49:21 -0300

    reference types

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
+import java.util.Date;
+
 public class Main {
    public static void main(String[] args) {
-    byte age = 27;
-    long viewsCount =  3_123_456_789L;
-    double price = 10.99;
-    float prices = 10.99F;
-    char letter = 'A';
-    boolean isEligible = false;
+    Date now = new Date();
+    System.out.println(now);
  }
 }
