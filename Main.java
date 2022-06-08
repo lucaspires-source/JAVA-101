294fddf53bad30bb16e848d6bb44621540986fa5
lucaspires-source <pires.lucas94@gmail.com>
2022-06-08 14:18:07 -0300

    Casting

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class Main {
    public static void main(String[] args) {
-      int result = 10 + 3;
-      System.out.println(result);
+      short x = 1;
+      int y = x + 2;
+      System.out.println(y);
  }
 }
