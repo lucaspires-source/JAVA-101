8931b39c6da25f1e4e501b20f4058258ef5fc34c
lucaspires-source <pires.lucas94@gmail.com>
2022-06-08 15:05:22 -0300

    symplifying if statments

--- Main.java
+++ Main.java
@@ -2,15 +2,9 @@
 
 public class Main {
    public static void main(String[] args) {
-      int temperature = 22;
+      int income = 120_000;
+      boolean hasHighIncome =  income > 100_00;
 
-      if(temperature > 30) {
-         System.out.println("It's a hot day");
-         System.out.println("Drink a lot of Water");
-      } else if ( temperature <= 30 && temperature >= 20 ) {
-         System.out.println("It is a nice day");
-      } else {
-         System.out.println("It is code");
-      }
+      System.out.println(hasHighIncome);
  }
 }
