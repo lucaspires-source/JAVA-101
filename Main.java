a7f8e8eab853881afdac71f0fc79b04d7804727c
lucaspires-source <pires.lucas94@gmail.com>
2022-06-08 15:13:54 -0300

    FizzBuzz

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner;
 
 public class Main {
    public static void main(String[] args) {
-      int income = 120_000;
-      boolean hasHighIncome =  income > 100_00;
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Number: ");
+      int number = scanner.nextInt();
 
-      System.out.println(hasHighIncome);
- }
+      if (number % 5 == 0){
+         if(number % 3 == 0) 
+         System.out.println("FizzBuzz");
+         else 
+            System.out.println("Fizz"); 
+      }else if (number % 3 == 0)
+         System.out.println("Buzz");
+         else 
+         System.out.println(number);
+ } 
 }
